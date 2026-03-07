07a844338f1b93c167b0ea0828641e6cc557c1fe
MapacheIngeniero <danielsanmiguelehnis@gmail.com>
2026-03-06 20:32:02 -0600

    Add functions for ASCII, binary, and Morse code

--- index.js
+++ index.js
@@ -1,1 +1,170 @@
+function BIN(){
+    const textos = document.getElementById("textos");
+    let texto = textos.value;
+let ascii = [];
+let binario = [];
 
+for (let i = 0; i < texto.length; i++) {
+    let codigo = texto.charCodeAt(i); // ASCII
+    ascii.push(codigo);
+
+    binario.push(codigo.toString(2).padStart(8, "0")); // binario 8 bits
+}
+
+console.log(ascii + " <-- Código ASCII");
+console.log(binario + " <-- Binario");
+}
+
+function CLEAR(){
+    console.clear();
+    const textos = document.getElementById("textos");
+    textos.value = "";
+}
+
+function MORS(){
+    const textos = document.getElementById("textos");
+    let texto = textos.value.toLowerCase();
+let morse = [];
+
+const morseCode = {
+    a: ".-", b: "-...", c: "-.-.", d: "-..", e: ".",
+    f: "..-.", g: "--.", h: "....", i: "..", j: ".---",
+    k: "-.-", l: ".-..", m: "--", n: "-.", o: "---",
+    p: ".--.", q: "--.-", r: ".-.", s: "...", t: "-",
+    u: "..-", v: "...-", w: ".--", x: "-..-", y: "-.--",
+    z: "--..",
+    "0": "-----","1": ".----","2": "..---","3": "...--",
+    "4": "....-","5": ".....","6": "-....","7": "--...",
+    "8": "---..","9": "----.",
+    "=": "-...-", " ": "/", "+": ".-.-.", "-": "-....-", "*": "..-",
+    "/": "-..-.", "(": "-.--.", ")": "-.--.-"
+};
+
+for (let i = 0; i < texto.length; i++) {
+    morse.push(morseCode[texto[i]] || "?");
+}
+
+console.log(morse.join(" ") + " <-- Código Morse");
+}
+
+function SUPER(){
+    const textos = document.getElementById("textos");
+    let texto = textos.value.toLowerCase();
+
+let ascii = [];
+let binario = [];
+let morse = [];
+let SUPER = [];
+const morseCode = {
+    a: ".-", b: "-...", c: "-.-.", d: "-..", e: ".",
+    f: "..-.", g: "--.", h: "....", i: "..", j: ".---",
+    k: "-.-", l: ".-..", m: "--", n: "-.", o: "---",
+    p: ".--.", q: "--.-", r: ".-.", s: "...", t: "-",
+    u: "..-", v: "...-", w: ".--", x: "-..-", y: "-.--",
+    z: "--..",
+    "0": "-----","1": ".----","2": "..---","3": "...--",
+    "4": "....-","5": ".....","6": "-....","7": "--...",
+    "8": "---..","9": "----.",
+    " ": "/"
+};
+
+const BINMORSE = {
+    ".-":"01100001", "-...":"01100010", "-.-.": "01100011", "-..": "01100100",
+    ".": "01100101", "..-.":"01100110", "--.": "01100111", "....": "01101000",
+    "..": "01101001", ".---":"01101010", "-.-": "01101011", ".-..": "01101100",
+    "--": "01101101", "-.": "01101110", "---": "01101111", ".--.": "01110000",
+    "--.-": "01110001", ".-.": "01110010", "...": "01110011", "-": "01110100",
+    "..-": "01110101", "...-": "01110101", ".--": "01110111", "-..-": "01111000",
+    "-.--": "01111001", "--..": "01111010"
+}
+
+const binmorse = {
+    
+}
+
+for (let i = 0; i < texto.length; i++) {
+    let char = texto[i];
+    let Super;
+    let SUPER1 = SUPER.push(BINMORSE[morseCode[char] || "?"]);
+
+    morse.push(morseCode[char] || "?");
+
+    let codigo = texto.charCodeAt(i);
+    ascii.push(codigo);
+
+    binario.push(codigo.toString(2).padStart(8, "0"));
+
+    
+    //SUPER.push(BINMORSE[char] || "?");
+
+    SUPER1;
+}
+//console.log("Binario:", binario);
+//console.log("Morse:", morse.join(" "));
+console.log("Super:", SUPER.join(" "));
+}
+
+let audioContext;
+let analyser;
+let data;
+
+async function iniciar(){
+
+    const stream = await navigator.mediaDevices.getUserMedia({audio:true});
+
+    audioContext = new AudioContext();
+
+    const source = audioContext.createMediaStreamSource(stream);
+
+    analyser = audioContext.createAnalyser();
+    source.connect(analyser);
+
+    data = new Uint8Array(analyser.frequencyBinCount);
+
+    detectar();
+
+}
+
+function detectar(){
+
+    setInterval(()=>{
+
+        analyser.getByteFrequencyData(data);
+
+        let suma = 0;
+
+        for(let i=0;i<data.length;i++){
+            suma += data[i];
+        }
+
+        let promedio = suma / data.length;
+
+        let bit = promedio > 20 ? 1 : 0;
+
+        document.getElementById("bitDetectado").textContent =
+        "Bit detectado: " + bit;
+
+    },100);
+
+}
+
+function generarBit(){
+
+    let bit = Math.round(Math.random());
+
+    let osc = audioContext.createOscillator();
+
+    osc.frequency.value = bit === 1 ? 900 : 400;
+
+    osc.connect(audioContext.destination);
+
+    osc.start();
+
+    setTimeout(()=>{
+        osc.stop();
+    },100);
+    bit = bit === 1 ? 0 : 1;
+    document.getElementById("bitGenerado").textContent =
+    "Último bit generado: " + bit;
+    console.log(bit);       
+}
